eb5cda2e4935876f415391090067f3314d3f311d
Thomas <60113615+caretinho@users.noreply.github.com>
2021-10-26 22:10:58 -0300

    01

--- factoryPattern.js
+++ factoryPattern.js
@@ -1,10 +1,16 @@
-function Developer(name)
+/*
+  Design Patterns -> Factory Pattern
+
+        EXAMPLE
+*/
+
+function Developer( name )
   {
     this.name = name
     this.type = "Developer"
   }
 
-function Tester(name)
+function Tester( name )
   {
     this.name = name
     this.type = "Tester"
@@ -12,30 +18,29 @@ function Tester(name)
 
 function Say()
   {
-    console.log("Hi i'm "+this.name+" and i'm a "+this.type)
+    console.log( "Hi i'm " + this.name + " and i'm a " + this.type )
   }
   
 function EmployeeFactory()
   {
   
-    this.create = (name, type) => {
+    this.create = ( name, type ) => {
       switch(type)
       
         {
           case 1:
           
-            return new Developer(name)
+            return new Developer( name )
           
           case 2:
           
-            return new Tester(name)
+            return new Tester( name )
           
           }
       }
   }
 
 const employeeFactory = new EmployeeFactory()
-
 const employees = []
 
 employees.push( employeeFactory.create( "Guy" , 1 ) )
@@ -43,6 +48,6 @@ employees.push( employeeFactory.create( "Other Guy" , 2 ) )
 
 employees.forEach( emp => 
   {
-    Say.call(emp)
+    Say.call( emp )
   }
 )
